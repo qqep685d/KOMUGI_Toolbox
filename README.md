07f79a33204f56486d6afd39e0cd00014fce843a
qqep685d@gmail.com <qqep685d@gmail.com>
2020-03-11 18:16:50 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# NBRP KOMUGI Toolbox
+
+## アプリケーション
+- FinDocs: ドキュメント管理データベースツール
+- Accounts: ユーザー機能
+
+
+## 参考
+- [開発用と本番用にDjangoの設定ファイルを分割する](https://medium.com/@kjmczk/django-multiple-settings-2a4c15c7c7b0)
+- [Herokuへのアップロード](https://tutorial-extensions.djangogirls.org/ja/heroku/)
+- [whitenoise 4.0を使う場合の注意点](https://qiita.com/ymhr1121/items/344c4eb300ab9972d0c2)
+
+
+- [ユーザー機能（ログイン、ログアウト）の実装](https://narito.ninja/blog/detail/39/)
---
+++ core/settings/production.py
@@ -0,0 +1,25 @@
+# settings/local.py
+
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'komugi_toolbox',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
