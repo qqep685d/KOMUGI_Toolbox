25d72f6715947fac7f997270a46e144c29546061
Atsushi Ohta <qqep685d@gmail.com>
2020-03-12 20:54:44 +0900

    delete STATIC_ROOT

--- core/settings.py
+++ core/settings.py
@@ -112,10 +112,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ローカル設定ファイルの読み込み
 try:
